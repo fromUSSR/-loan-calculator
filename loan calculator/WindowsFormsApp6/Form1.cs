3a0dc0223195e4c1cd6733014766adb3c8ddfc28
fromUSSR <62261546+fromUSSR@users.noreply.github.com>
2020-03-18 17:09:12 +0300

    Add files via upload

---
+++ loan calculator/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public int a;
+        public int b;
+        public int c;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            if (radioButton1.Checked == true)
+            {
+                c = a + b;
+                textBox3.Text = Convert.ToString(c);
+            }
+            else if (radioButton2.Checked == true)
+            {
+                c = a - b;
+                textBox3.Text = Convert.ToString(c);
+            }
+            else if (radioButton3.Checked == true)
+            {
+                c = a * b;
+                textBox3.Text = Convert.ToString(c);
+            }
+            else if (radioButton4.Checked == true)
+            {
+                c = a / b;
+                textBox3.Text = Convert.ToString(c);
+            }
+        }
+    }
+}
